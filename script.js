00ae519d058fd7f6b5959b7a1d4e3150dc19321b
Denis <legion1259@gmail.com>
2021-01-19 19:36:45 +0300

    massive-method

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const arr = [12, 33, 2, 26, 18];
+arr.sort();
+console.log(arr);
+
+// arr[99] = 0;
+// console.log(arr.length);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`)
+// });
+
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join(';'));
